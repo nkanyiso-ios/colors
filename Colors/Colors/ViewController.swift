dc9564e457b390d030888379d35bb0a7d86c4b70
nkanyisoik <nkanyiso@ikhokha.com>
2021-06-05 16:49:04 +0200

    initial project create

---
+++ Colors/Colors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Nkanyiso Hlela on 2021/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
