ad73ad136591f91a2c5cd5b0f342ad90a9482dda
nkanyisoik <nkanyiso@ikhokha.com>
2021-06-05 17:57:17 +0200

    Added color service api

---
+++ Colors/Colors/APIService/ColorApiService.swift
@@ -0,0 +1,16 @@
+//
+//  ColorApiService.swift
+//  Colors
+//
+//  Created by Nkanyiso Hlela on 2021/06/05.
+//
+
+import Foundation
+
+protocol ColorApiServiceProtocol {func convertRGBColorToHex()}
+
+class ColorApiService: ColorApiServiceProtocol {
+    func convertRGBColorToHex() {
+        
+    }
+}
