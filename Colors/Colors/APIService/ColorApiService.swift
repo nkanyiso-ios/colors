c093c2093a197097c052e406312dedf28f85ea79
nkanyisoik <nkanyiso@ikhokha.com>
2021-06-05 18:48:26 +0200

    implemented the get color service

--- Colors/Colors/APIService/ColorApiService.swift
+++ Colors/Colors/APIService/ColorApiService.swift
@@ -8,10 +8,23 @@
 import Foundation
 import Alamofire
 
-protocol ColorApiServiceProtocol {func convertRGBColorToHex()}
+protocol ColorApiServiceProtocol {func convertRGBColorToHex(url:String ,method:HTTPMethod,_ completion: @escaping (Result<Data, Error>) -> Void)}
 
 class ColorApiService: ColorApiServiceProtocol {
-    func convertRGBColorToHex() {
-        
+    func convertRGBColorToHex(url:String ,method:HTTPMethod,_ completion: @escaping (Result<Data, Error>) -> Void) {
+        AF.request(url, method: .get).response{ response in
+            
+            switch (response.result) {
+            case .success:
+                if let data = response.data {
+                    completion(.success(data))
+                }else{
+                    completion(.failure(RequestError.failedNoData))
+                }
+            case .failure( let error):
+                completion(.failure(error))
+                print(error)
+            }
+        }
     }
 }
---
+++ Colors/Colors/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Colors
+//
+//  Created by Nkanyiso Hlela on 2021/06/05.
+//
+
+import Foundation
+enum RequestError: Error {
+    case invalidURL
+    case failedNoData
+}
