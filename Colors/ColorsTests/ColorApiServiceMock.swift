981d5e693c13260e38172cfed9b53aabe4bc79c8
nkanyisoik <nkanyiso@ikhokha.com>
2021-06-06 17:10:47 +0200

    Created unit tests

---
+++ Colors/ColorsTests/ColorApiServiceMock.swift
@@ -0,0 +1,19 @@
+//
+//  ColorApiServiceMock.swift
+//  ColorsTests
+//
+//  Created by Nkanyiso Hlela on 2021/06/06.
+//
+
+import Foundation
+@testable import Colors
+class ColorApiServiceMock: ColorApiServiceProtocol {
+    func convertRGBColorToHex(rgbColor: String, _ completion: @escaping (Result<ColorResponse, Error>) -> Void) {
+        if(rgbColor == "255,255,255"){
+            completion(.success(ColorResponse(hex: hexValue(value: "#FFFF", clean: "FFFF"))))
+        }else{
+            completion(.failure(RequestError.failedNoData))
+        }
+    }
+    
+}
