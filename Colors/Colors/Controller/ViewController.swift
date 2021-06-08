5dbabefa5fd41cedac90e2c773a459d79e593a34
Nkanyiso <13.nkanyiso@gmail.com>
2021-06-08 16:32:41 +0200

    UI update and some code cleanup

--- Colors/Colors/Controller/ViewController.swift
+++ Colors/Colors/Controller/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
-    var viewModel = ColorViewModel(colorApiService: ColorApiService())
-    var cancellable: AnyCancellable?
+    private lazy var viewModel = ColorViewModel(colorApiService: ColorApiService())
+    private var cancellable: AnyCancellable?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidLoad()
@@ -19,44 +19,58 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.cancellable = viewModel.$state.receive(on: DispatchQueue.main).sink(receiveValue: { state in
+        colorPreview.layer.cornerRadius = 10
+        self.cancellable = viewModel.$state.receive(on: DispatchQueue.main).sink(receiveValue: { [weak self] state in
             switch state{
             case .idle:
                 print("Initialized viewmodel")
             case .fetching:
-                self.view.activityStartAnimating(activityColor: UIColor.white, backgroundColor: UIColor.black.withAlphaComponent(0.5))
+                self?.view.activityStartAnimating(activityColor: UIColor.white, backgroundColor: UIColor.black.withAlphaComponent(0.5))
             case .fetched(let hexValue):
-                self.updateHexValue(hexString: hexValue.value)
-                self.view.activityStopAnimating()
+                self?.updateHexValue(hexString: hexValue.value)
+                
+                self?.colorPreview.transform = CGAffineTransform.init(scaleX: 0.5, y: 0.5)
+                UIView.animate(withDuration: 0.4,
+                               delay: 0.0,
+                               options: .curveEaseInOut) { [weak self] in
+                    self?.colorPreview.alpha = 1
+                    self?.colorPreview.transform = .identity
+                } completion: { _ in }
+                
+                UIView.animate(withDuration: 0.6) { [weak self] in
+                    self?.colorPreview.alpha = 1
+                    self?.colorPreview.transform = .identity
+                }
+                self?.view.activityStopAnimating()
             case .error(let error):
-                self.view.activityStopAnimating()
-                self.showBasicAlert(alertTitle: NSLocalizedString("Colors", comment: ""), alertMessage: error.localizedDescription)
+                self?.view.activityStopAnimating()
+                self?.showBasicAlert(alertTitle: NSLocalizedString("Colors", comment: ""), alertMessage: error.localizedDescription)
             }
         })
-        
     }
     
-    deinit {
-        self.cancellable?.cancel()
-    }
+    deinit { self.cancellable?.cancel() }
     
-    func updateHexValue(hexString: String){
+    private func updateHexValue(hexString: String) {
         hexValueLabel.text = " \(NSLocalizedString("The hex value : ", comment: "")) \(hexString)"
     }
     
-    @IBAction func pickColor(_ sender: Any) {
+    @IBAction private func pickColor(_ sender: Any) {
         let picker = UIColorPickerViewController()
         picker.delegate = self
         self.present(picker, animated: true, completion: nil)
     }
     
-    @IBOutlet weak var hexValueLabel: UILabel!
+    @IBOutlet private weak var hexValueLabel: UILabel!
+    @IBOutlet private weak var colorPreview: UIView!
 }
 extension ViewController: UIColorPickerViewControllerDelegate {
     
     func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
         let selectedColor = viewController.selectedColor
+        colorPreview.backgroundColor = selectedColor
+        colorPreview.alpha = 0
+        
         viewModel.getHexValue(rgbColor: selectedColor.rgbString() ?? "")
     }
     
